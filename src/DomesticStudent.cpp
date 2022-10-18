32137af74b62b66f71a7be491b26da351b0b8465
rrennn08 <66759042+rrennn08@users.noreply.github.com>
2022-10-17 23:53:08 -0700

    overloaded << operator for DomesticStudent

--- src/DomesticStudent.cpp
+++ src/DomesticStudent.cpp
@@ -1,1 +1,20 @@
 #include "DomesticStudent.hpp"
+
+//overload <<
+std::ostream& operator<<(std::ostream& os, const DomesticStudent& stu){
+	//Example: Domestic Student #: 301461164, Fok, Darren, Province: BC, CGPA: 3.14, Research Score: 25
+	//output
+	os << "Domestic Student #: " << stu.getID() << ", " << stu.getLastName << ", " << stu.getFirstName << ", Province: " <<
+			stu.getProvince << ", CGPA: " << stu.getCGPA << ", Research Score: " << stu.getResearch << std::endl;
+
+	return os;
+}
+
+//getter and setter
+void DomesticStudent::setProvince(string input){
+	province = input;
+}
+
+string DomesticStudent::getProvince() const{
+	return province;
+}
--- src/DomesticStudent.hpp
+++ src/DomesticStudent.hpp
@@ -3,8 +3,11 @@
 //DomesticStudent child class of Student
 class DomesticStudent : public Student{
 public:
+	//overload <<
+	friend std::ostream& operator<<(std::ostream& os, const Student& stu);
+
 	//province
-	void SetProvince(string province);
+	void setProvince(string province);
 	string getProvince() const;
 
 private:
