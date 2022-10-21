16c8d1eed7822100ff07f526b7dc3e3ef6ba25a5
rrennn08 <66759042+rrennn08@users.noreply.github.com>
2022-10-20 20:00:32 -0700

    work on sort.cpp/hpp

--- src/stu_sort.cpp
+++ src/stu_sort.cpp
@@ -1,3 +1,13 @@
 //stu_sort.cpp to implement your sorting functions
 #include "stu_sort.hpp"
 
+vector<Student> studentSort::sortLastName(vector<Student> input){
+
+}
+
+void studentSort::swap(Student& input1, Student& input2){
+	Student temp; //temp student object to hold info
+	temp = input1;
+	input1 = input2;
+	input2 = temp;
+}
--- src/stu_sort.hpp
+++ src/stu_sort.hpp
@@ -1,2 +1,12 @@
 //header file stu_sort.hpp to declare your sorting functions
 #include <string> //you will have to use string in C++
+#include "vector" //vectors
+#include "DomesticStudent.hpp"
+#include "InternationalStudent.hpp"
+#include <iostream>
+using namespace std;
+
+namespace studentSort{ //make it namespace, so that functions can be accessed outside
+	vector<Student> sortLastName(vector<Student> input); //sort function by Darren
+	void swap(Student& input1, Student& input2); //function to swap
+}
