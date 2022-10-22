b230bd44d54b76dacfd6644a4da939d7ab7043a7
Leon <75389253+LeonChen2@users.noreply.github.com>
2022-10-21 22:01:00 -0700

    Update stu_sort.hpp

--- src/stu_sort.hpp
+++ src/stu_sort.hpp
@@ -8,6 +8,12 @@ namespace studentSort{ //make it namespace, so that functions can be accessed ou
 	//lastName sort
 	int partition1(vector<Student> array, int first, int last); //from low to high
 	void sortLastName(vector<Student> array, int first, int last);
+	//firstname sort
+	void sortFirstName(vector<Student> array, int first, int last);
+	//cgpa sort
+	void sortCGPA(vector<Student> array, int first, int last);
+	//researchscore sort
+	void sortResearchScore(vector<Student> array, int first, int last);
 
 	void swap(Student& input1, Student& input2); //function to swap
 
