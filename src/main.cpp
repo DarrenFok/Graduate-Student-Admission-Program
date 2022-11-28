9c647f4040f766abf69bfe118e0e97009ce97b04
Leon <75389253+LeonChen2@users.noreply.github.com>
2022-11-27 22:31:08 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,14 @@
 #include "stu_sort.hpp"
 #include "toefl.hpp"
 #include "student.hpp"
+#include "functionTwo.hpp"
+using namespace functionTwo;
 using namespace std;
 
 int main() {
     string menuChoice; //to choose domestic, international, exit
-    string menuChoice2; //to choose what type of sort
+    string menuChoice2; //to choose what to do
+    string menuChoice3, menuChoice4, menuChoice5;
     string line;
 
     //read .txt files
@@ -29,8 +32,10 @@ int main() {
         cout << "ERROR: Unable to open file international-stu.txt";
         return -1;
     }
-    
-    //input domesticStudents
+
+
+    //                                      Inputting Domestic Students
+    //---------------------------------------------------------------------------------------------------------
     getline(domesticFile, line);
     int domesticCount = 0;
     DomesticList DL1;
@@ -64,91 +69,231 @@ int main() {
         DL1.sortedInsert(firstName, lastName, cgpa, researchScore, DL1.getDomesticID(),province);
         domesticCount++;
         DL1.setDomesticID(DL1.getDomesticID() + 1);
-
-
     }
 
-//   DL1.display();
-//    cout << "\ntesting delete\n";
-//    DL1.selectDelete();
-//    DL1.display();;
-//    cout << endl << endl;
-//    DL1.display();
-//    cout << endl;
-//    DL1.displayHead();
-//    DL1.displayTail();
+//                                      Inputting International Students
+//--------------------------------------------------------------------------------------------
+    getline(internationalFile, line);
+    int internationalCount = 0;
+    InternationalList LI;
+    while (getline(internationalFile, line)){
+        istringstream ss(line);
+        string firstName, lastName, country, s_cgpa, s_researchScore, reading, listening, speaking, writing;
+        float cgpa;
+        int researchScore;
+
+        getline(ss, firstName, ','); //get firstName
+        getline(ss, lastName, ','); //get lastName
+        getline(ss, country, ','); //get province
+        getline(ss, s_cgpa, ','); //get cgpa
+        getline(ss, s_researchScore, ','); //get researchScore
+        getline(ss, reading, ','); //set toefl reading
+        getline(ss, listening, ','); //set toefl listening
+        getline(ss, speaking, ','); //set toefl speaking
+        getline(ss, writing, ','); //set toefl writing
+
+        if(firstName == "" || lastName == "" || country == "" || s_cgpa == "" || s_researchScore == "" || reading == "" ||
+        listening == "" || speaking == "" || writing == ""){
+            cout << "ERROR: One field in this line is missing. Exiting program now" << endl;
+            exit(1);
+        }
+
+        if(country == "Idian"){ //correct spelling error
+            country = "India";
+        }
+
+        if(country != "Canada" && country != "China" && country != "India" && country != "Iran" && country != "Korea"){ //checking if valid country
+            cout << "ERROR: Country input is invalid. Exiting the program now" << endl;
+            exit(1);
+        }
+
+        //create toefl object
+        toefl score;
+        score.setReading(stoi(reading));
+        score.setListening(stoi(listening));
+        score.setSpeaking(stoi(speaking));
+        score.setWriting(stoi(writing));
+        score.setTotal();
+
+        cgpa = stof(s_cgpa);
+        researchScore = stoi(s_researchScore);
+
+        if(score.getReading() > 19 && score.getListening() > 19 && score.getSpeaking() > 19 &&
+                 score.getWriting() > 19 && score.getTotal() > 92) {
+             LI.sortedInsert(firstName, lastName, cgpa, researchScore, LI.getIntID(), country, score);
+        }
+        internationalCount++;
+        LI.setIntID(LI.getIntID() + 1);
+    }
+
+    // MergeList MI;
+    // MI.sortedMerge(DL1, LI);
+    // MI.display();
+    // MI.search();
+
+
+//                                             Graudation Adimission V2.0
+    //----------------------------------------------------------------------------------------------------------
+    cout << "\nWelcome to Graduate Studemt Admission System Version 2.0\n";
+    do{
+        cout << "\nMain Menu\n";
+        cout << "Select which of the following you would like by entering respective number: \n";
+        cout << "1) Domestic Students\n";
+        cout << "2) International Students\n";
+        cout << "3) Merge Domestic and International Students\n";
+        cout << "0) Exit program\n";
+        cin >> menuChoice;
+        //check user input for errors
+        if(functionTwo::isNumerical(menuChoice) == false){ //check for number inputs only
+            cout << "ERROR: Invalid input, numbers accepted only\n";
+            continue;
+        }
+        else if(stoi(menuChoice) > 3 || stoi(menuChoice) < 0){ //check for valid numbers only
+            cout << "ERROR: Invalid number, please input 0-3\n";
+            continue;
+            }
+        else if(menuChoice == "1"){
+            cout << "\nDomestic Student List Menu\n";
+            cout <<"Enter the following number of the option you would like\n";
+            cout <<"1) Search Student based on Student Objects\n";
+            cout <<"2) Search Student based on Student Name\n";
+            cout <<"3) Insert a new Student\n";
+            cout <<"4) Delete Student\n";
+            cout <<"5) Delete the head and tail of Student list\n";
+            cout <<"6) Return back to Main Menu\n";
+            cin >> menuChoice2;
+                if(functionTwo::isNumerical(menuChoice2) == false){ //check for number inputs only
+                    cout << "ERROR: Invalid input, numbers accepted only\n";
+                    continue;
+                }
+                else if(stoi(menuChoice2) > 6 || stoi(menuChoice2) < 0){ //check for valid numbers only
+                    cout << "ERROR: Invalid number, please input 0-6\n";
+                    continue;
+                }
+                else{
+                    if(menuChoice2 == "1"){
+                        DL1.searchOne();
+                    }
+                    else if(menuChoice2 == "2"){
+                        DL1.searchTwo();
+                    }
+                    else if (menuChoice2 == "3"){
+                        DL1.create();
+                    }
+                    else if(menuChoice2 == "4"){
+                        DL1.selectDelete();
+                    }
+                    else if(menuChoice2 == "5"){
+                        DL1.deleteHeadTail();
+                    }
+                }
+        }
+        else if(menuChoice == "2"){
+            cout << "\nInternational Student List Menu\n";
+            cout <<"Enter the following number of the option you would like\n";
+            cout <<"1) Search Student based on Student Objects\n";
+            cout <<"2) Search Student based on Student Name\n";
+            cout <<"3) Insert a new Student\n";
+            cout <<"4) Delete Student\n";
+            cout <<"5) Delete the head and tail of Student list\n";
+            cout <<"6) Return back to Main Menu\n";
+            cin >> menuChoice3;
+            if(functionTwo::isNumerical(menuChoice3) == false){ //check for number inputs only
+                    cout << "ERROR: Invalid input, numbers accepted only\n";
+                    continue;
+            }
+            else if(stoi(menuChoice2) > 6 || stoi(menuChoice2) < 0){ //check for valid numbers only
+                cout << "ERROR: Invalid number, please input 0-6\n";
+                continue;
+            }
+            else{
+                    if(menuChoice2 == "1"){
+                        LI.searchOne();
+                    }
+                    else if(menuChoice2 == "2"){
+                        LI.searchTwo();
+                    }
+                    else if (menuChoice2 == "3"){
+                        LI.create();
+                    }
+                    else if(menuChoice2 == "4"){
+                        LI.selectedDelete();
+                    }
+                    else if(menuChoice2 == "5"){
+                        LI.deleteHeadTail();
+                    }
+                }
+        }
+    }
+    while(menuChoice != "0");
+
+
+}
+
+
+
+    //----------------------------------------------------------------------------------------------------------
 
      //DL1.searchOne();
 
 //   //input internationalStudents
-     getline(internationalFile, line);
-     int internationalCount = 0;
-     InternationalList LI;
-     while (getline(internationalFile, line)){
-         istringstream ss(line);
-         string firstName, lastName, country, s_cgpa, s_researchScore, reading, listening, speaking, writing;
-         float cgpa;
-         int researchScore;
+    // getline(internationalFile, line);
+    // int internationalCount = 0;
+    // InternationalList LI;
+    // while (getline(internationalFile, line)){
+    //     istringstream ss(line);
+    //     string firstName, lastName, country, s_cgpa, s_researchScore, reading, listening, speaking, writing;
+    //     float cgpa;
+    //     int researchScore;
 
-         getline(ss, firstName, ','); //get firstName
-         getline(ss, lastName, ','); //get lastName
-         getline(ss, country, ','); //get province
-         getline(ss, s_cgpa, ','); //get cgpa
-         getline(ss, s_researchScore, ','); //get researchScore
-         getline(ss, reading, ','); //set toefl reading
-         getline(ss, listening, ','); //set toefl listening
-         getline(ss, speaking, ','); //set toefl speaking
-         getline(ss, writing, ','); //set toefl writing
+    //     getline(ss, firstName, ','); //get firstName
+    //     getline(ss, lastName, ','); //get lastName
+    //     getline(ss, country, ','); //get province
+    //     getline(ss, s_cgpa, ','); //get cgpa
+    //     getline(ss, s_researchScore, ','); //get researchScore
+    //     getline(ss, reading, ','); //set toefl reading
+    //     getline(ss, listening, ','); //set toefl listening
+    //     getline(ss, speaking, ','); //set toefl speaking
+    //     getline(ss, writing, ','); //set toefl writing
 
-         if(firstName == "" || lastName == "" || country == "" || s_cgpa == "" || s_researchScore == "" || reading == "" ||
-         listening == "" || speaking == "" || writing == ""){
-             cout << "ERROR: One field in this line is missing. Exiting program now" << endl;
-             exit(1);
-         }
+    //     if(firstName == "" || lastName == "" || country == "" || s_cgpa == "" || s_researchScore == "" || reading == "" ||
+    //     listening == "" || speaking == "" || writing == ""){
+    //         cout << "ERROR: One field in this line is missing. Exiting program now" << endl;
+    //         exit(1);
+    //     }
 
-         if(country == "Idian"){ //correct spelling error
-             country = "India";
-         }
+    //     if(country == "Idian"){ //correct spelling error
+    //         country = "India";
+    //     }
 
-         if(country != "Canada" && country != "China" && country != "India" && country != "Iran" && country != "Korea"){ //checking if valid country
-             cout << "ERROR: Country input is invalid. Exiting the program now" << endl;
-             exit(1);
-         }
+    //     if(country != "Canada" && country != "China" && country != "India" && country != "Iran" && country != "Korea"){ //checking if valid country
+    //         cout << "ERROR: Country input is invalid. Exiting the program now" << endl;
+    //         exit(1);
+    //     }
 
-         //create toefl object
-         toefl score;
-         score.setReading(stoi(reading));
-         score.setListening(stoi(listening));
-         score.setSpeaking(stoi(speaking));
-         score.setWriting(stoi(writing));
-         score.setTotal();
+    //     //create toefl object
+    //     toefl score;
+    //     score.setReading(stoi(reading));
+    //     score.setListening(stoi(listening));
+    //     score.setSpeaking(stoi(speaking));
+    //     score.setWriting(stoi(writing));
+    //     score.setTotal();
 
-         cgpa = stof(s_cgpa);
-         researchScore = stoi(s_researchScore);
+    //     cgpa = stof(s_cgpa);
+    //     researchScore = stoi(s_researchScore);
 
-         if(score.getReading() > 19 && score.getListening() > 19 && score.getSpeaking() > 19 &&
-                  score.getWriting() > 19 && score.getTotal() > 92) {
-              LI.sortedInsert(firstName, lastName, cgpa, researchScore, LI.getIntID(), country, score);
-             internationalCount++;
-         }
-         LI.setIntID(LI.getIntID() + 1);
-     }
-//     LI.display();
-//     LI.create();
-//     LI.display();
-//     LI.selectedDelete();
-//     LI.display();
-// cout << internationalCount;
+    //     if(score.getReading() > 19 && score.getListening() > 19 && score.getSpeaking() > 19 &&
+    //              score.getWriting() > 19 && score.getTotal() > 92) {
+    //          LI.sortedInsert(firstName, lastName, cgpa, researchScore, LI.getIntID(), country, score);
+    //     }
+    //     internationalCount++;
+    //     LI.setIntID(LI.getIntID() + 1);
+    // }
+    //  LI.display();
+    //  LI.displayHead();
+    //  LI.displayTail();
 
-    MergeList MI;
-    MI.sortedMerge(DL1, LI);
-    MI.display();
-    MI.search();
-
-
-
-
-
+    // LI.searchTwo();
 
     // cout << "\nWelcome to the Graduate Student Admission Program\n";
     // //menu - loops until user wants to exit
@@ -310,8 +455,3 @@ int main() {
     // }
     // while(menuChoice != "0"); //exiting program if 0
     //cout << "Thank you for using the program\n";
-
-
-  return 0;
-
-}
