f349543b13ea30fe9c3f28e4675ee6a6c95c7c05
Leon <75389253+LeonChen2@users.noreply.github.com>
2022-11-28 19:32:32 -0800

    Update UnitTest.hpp

--- src/UnitTest.hpp
+++ src/UnitTest.hpp
@@ -8,8 +8,23 @@ namespace UnitTest{
     void TestInsert(){
         //test 1
         DomesticList test;
+        cout <<"\nDomestic inserting Normal Case: first, last, 4.33, 100, 123456789, BC\n";
         test.sortedInsert("first", "last", "4.33", "100", 123456789, "BC");
+        cout <<"\nDomestic inserting Corner Case: first, last, 5.33, 110, 123456789, FAKEPROVINCE\n";
+        test.sortedInsert("first", "last", "5.33", "110", 123456789, "FAKEPROVINCE");
+        cout <<"\nDomestic inserting Illegal Case: first, last, four, hundred, 123456789, 123\n";
+        test.sortedInsert("first", "last", "four", "hundred", 123456789, "123");
         test.display();
+
+        InternationalList Test;
+        // cout <<"\nInternational inserting Normal Case: first, last, 4.33, 100, 123456789, China, 50\n";
+        // Test.sortedInsert("first", "last", 4.33, 100, 123456789, "China", 50);
+        // cout <<"\nInternational inserting Corner Case: first, last, 5.33, 110, 123456789, FAKECOUNTRY, 200\n";
+        // Test.sortedInsert("first", "last", 5.33, 110, 123456789, "FAKEPROVINCE", 200);
+        // cout <<"\nInternational inserting Illegal Case: first, last, four, hundred, 123456789, 123, Fail\n";
+        // Test.sortedInsert("first", "last", "four", "hundred", 123456789, "123", "Fail");
+        // Test.display();
+
 
         //test 2
 
